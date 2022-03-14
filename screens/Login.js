88e5f3e8db1fab43c4335b48d711f36cdc560af0
WDowns2 <77688462+WDowns2@users.noreply.github.com>
2022-03-14 12:53:44 +0000

    Create Login.js

---
+++ screens/Login.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Button } from 'react-native';
+import { ScrollView, TextInput } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+class Login extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    login = async () => {
+
+        //Validation here...
+
+        return fetch("http://localhost:3333/api/1.0.0/login", {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(this.state)
+        })
+        .then((response) => {
+            if(response.status === 200){
+                return response.json()
+            }else if(response.status === 400){
+                throw 'Invalid email or password';
+            }else{
+                throw 'Something went wrong';
+            }
+        })
+        .then(async (responseJson) => {
+                console.log(responseJson);
+                await AsyncStorage.setItem('@session_id', responseJson.id);
+                await AsyncStorage.setItem('@session_token', responseJson.token);
+                this.props.navigation.navigate("Home");
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+    }
+
+    render(){
+        return (
+            <ScrollView>
+                <TextInput
+                    placeholder="Enter your email..."
+                    onChangeText={(email) => this.setState({email})}
+                    value={this.state.email}
+                    style={{padding:5, borderWidth:1, margin:5}}
+                />
+                <TextInput
+                    placeholder="Enter your password..."
+                    onChangeText={(password) => this.setState({password})}
+                    value={this.state.password}
+                    secureTextEntry
+                    style={{padding:5, borderWidth:1, margin:5}}
+                />
+                <Button
+                    title="Login"
+                    onPress={() => this.login()}
+                />
+                <Button
+                    title="Don't have an account?"
+                    color="darkblue"
+                    onPress={() => this.props.navigation.navigate("Signup")}
+                />
+            </ScrollView>
+        )
+    }
+}
+
+export default Login;
